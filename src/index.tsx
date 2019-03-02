84544222635b8d5487b1234d99a7b6f32e67df81
Paulo Vaz <paulopontovaz@hotmail.com>
2019-03-02 10:24:52 +0100

    Rough structure setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './assets/styles/index.scss';
+import App from './components/app';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './store/reducers';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/store/reducers/index.ts
@@ -0,0 +1,9 @@
+
+function repos(state: Array<any> = [], action: any) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default repos;
